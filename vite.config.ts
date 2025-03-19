ec6c759cd4fa333c80a4cbfe610d26c9c2a37f5b
alireza-abdii <alirezaabdichari@gmail.com>
2025-03-20 02:19:12 +0330

    fix tailwind not working issue

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   css: {
-    postcss: {},
+    postcss: "./postcss.config.cjs",
   },
   server: {
     port: 3000,
